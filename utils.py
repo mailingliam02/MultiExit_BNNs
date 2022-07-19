a84f7c2ebf8f9f7519c812d75903f11ef4b0987d
Liam Castelli <liam.castelli21@imperial.ac.uk>
2022-07-19 03:14:51 +0100

    Added dict_drop to utils

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ from pathlib import Path
 
 PROJ_DIR = Path(os.path.realpath(__file__)).parent
 RUNS_DB_DIR = PROJ_DIR / 'runs_db'
+
+def dict_drop(dic, *keys):
+    new_dic = dic.copy()
+    for key in keys:
+        if key in new_dic:
+            del new_dic[key]
+    return new_dic
