69df261b5252fff45fb4d369fe2080c0db348e04
Liam Castelli <liam.castelli21@imperial.ac.uk>
2022-07-24 02:04:27 +0100

    Added resnet loss capability in loss_loader

--- to_train/loss/loss_loader.py
+++ to_train/loss/loss_loader.py
@@ -1,8 +1,12 @@
 import to_train.loss.msdnet_loss as loss
+import to_train.loss.resnet_loss as resnet_loss
 from to_train.loss.loss_utils import dict_drop
 
 # Will need to add some functionality if it is not one of the msdnet_losses!
 def get_loss_function(hyperparameters):
-    loss_f = getattr(loss, hyperparameters['call'])(**dict_drop(hyperparameters, 'call'))
+    if hyperparameters['call'] == 'ExitEnsembleDistillation':
+        loss_f = getattr(resnet_loss, hyperparameters['call'])(**dict_drop(hyperparameters, 'call'))
+    else:
+        loss_f = getattr(loss, hyperparameters['call'])(**dict_drop(hyperparameters, 'call'))
     return loss_f
 
