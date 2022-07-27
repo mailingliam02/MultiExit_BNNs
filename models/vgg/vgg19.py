fc6650bfae45ded8075f10485df2d94bef998859
Liam Castelli <liam.castelli21@imperial.ac.uk>
2022-07-27 02:19:39 +0100

    Updated VGG to fix bugs and have better init

--- models/vgg/vgg19.py
+++ models/vgg/vgg19.py
@@ -6,7 +6,9 @@
 '''VGG11/13/16/19 in Pytorch.'''
 
 import torch
+import math
 import torch.nn as nn
+import torch.nn.functional as F
 from utils import dict_drop
 
 def get_vgg_19(ensemble, network_hyperparams):
@@ -39,14 +41,28 @@ class VGG(nn.Module):
         super().__init__()
         self.blocks, self.non_sequentialized_blocks = blocks
 
-        self.non_sequentialized_classifier = [nn.Linear(512, 4096),
+        self.non_sequentialized_classifier = nn.ModuleList(modules = [nn.Linear(512, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(4096, num_class)]
+            nn.Linear(4096, num_class)])
         self.classifier = nn.Sequential(*self.non_sequentialized_classifier)
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                #torch.nn.init.kaiming_uniform_(m.weight, a=0, mode='fan_in', nonlinearity='relu')
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                torch.nn.init.xavier_uniform_(m.weight)
+                m.bias.data.fill_(0.01)
 
     def forward(self, x):
         output = x
@@ -58,15 +74,15 @@ class VGG(nn.Module):
         return [output]
 
 def make_layers(cfg, batch_norm=False):
-    blocks = []
-    layers = []
+    blocks = nn.ModuleList()
+    layers = nn.ModuleList()
 
     input_channel = 3
     for l in cfg:
         if l == 'M':
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            blocks += layers
-            layers = []
+            blocks.append(layers)
+            layers = nn.ModuleList()
             continue
 
         layers += [nn.Conv2d(input_channel, l, kernel_size=3, padding=1)]
@@ -85,9 +101,10 @@ def make_layers(cfg, batch_norm=False):
 class VGG19(VGG):
     def __init__(self, n_exits = 1, out_dim = 100, *args,  **kwargs):
         cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
-        super().__init__(make_layers(cfg, batch_norm=True), num_classes=out_dim, *args,  **kwargs)
+        super().__init__(make_layers(cfg, batch_norm=True), num_class=out_dim, *args,  **kwargs)
         self.n_exits = n_exits
         self.out_dim = out_dim
+        self.init_weights()
 
 class VGG19MC(VGG19):
     def __init__(self,dropout_exit = False, dropout = None, dropout_p = 0.5, n_exits = 1, out_dim = 100, *args,  **kwargs):
@@ -121,6 +138,8 @@ class VGG19MC(VGG19):
                     self.non_sequentialized_classifier[layer] = dropout_layer
             # Overwrite classifier from super class
             self.classifier = nn.Sequential(*self.non_sequentialized_classifier)
+        
+        self.init_weights()
 
 class VGG19EarlyExit(VGG19):
     def __init__(self,n_exits = 4, out_dim = 100, *args,  **kwargs):
@@ -134,9 +153,9 @@ class VGG19EarlyExit(VGG19):
         ex1relu1 = nn.ReLU(inplace=True)
         ex1relu2 = nn.ReLU(inplace=True)
         ex1relu3 = nn.ReLU(inplace=True)
-        ex1featureextractor_list = [ex1conv1,ex1bn1,ex1relu1,ex1conv2,ex1bn2,ex1relu2,ex1conv3,ex1bn3,ex1relu3]
+        ex1featureextractor_list = nn.ModuleList(ex1conv1,ex1bn1,ex1relu1,ex1conv2,ex1bn2,ex1relu2,ex1conv3,ex1bn3,ex1relu3)
         self.ex1featureextractor = nn.Sequential(*ex1featureextractor_list)
-        self.ex1linear = nn.Linear(512, num_classes)
+        self.ex1linear = nn.Linear(512, out_dim)
 
         ex2conv1 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=False)
         ex2conv2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False)
@@ -144,26 +163,27 @@ class VGG19EarlyExit(VGG19):
         ex2bn2 = nn.BatchNorm2d(512)
         ex2relu1 = nn.ReLU(inplace=True)
         ex2relu2 = nn.ReLU(inplace=True)
-        ex2featureextractor_list = [ex2conv1,ex2bn1,ex2relu1,ex2conv2,ex2bn2,ex2relu2]
+        ex2featureextractor_list = nn.ModuleList(ex2conv1,ex2bn1,ex2relu1,ex2conv2,ex2bn2,ex2relu2)
         self.ex2featureextractor = nn.Sequential(*ex2featureextractor_list)
-        self.ex2linear = nn.Linear(512, num_classes)
+        self.ex2linear = nn.Linear(512, out_dim)
 
 
         ex3conv1 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False)
         ex3bn1 = nn.BatchNorm2d(512)
         ex3relu1 = nn.ReLU(inplace=True)
-        ex3featureextractor_list = [ex3conv1,ex3bn1,ex3relu1]
+        ex3featureextractor_list = nn.ModuleList(ex3conv1,ex3bn1,ex3relu1)
         self.ex3featureextractor = nn.Sequential(*ex3featureextractor_list)
-        self.ex3linear = nn.Linear(512, num_classes)
+        self.ex3linear = nn.Linear(512, out_dim)
 
-        self.ex4linear = nn.Linear(512, num_classes)
+        self.ex4linear = nn.Linear(512, out_dim)
 
+        self.init_weights()
 
     def forward(self, x):
         out = self.blocks[0](x)
 
         out1 = self.ex1featureextractor(F.relu(out))
-        out1 = F.avg_pool2d(out1, 4)
+        out1 = F.avg_pool2d(out1, 2)
         middle1_fea = out1
         out1 = out1.view(out1.size(0), -1)
         out1 = self.ex1linear(out1)
@@ -171,7 +191,7 @@ class VGG19EarlyExit(VGG19):
         out = self.blocks[1](out)
 
         out2 = self.ex2featureextractor(F.relu(out))
-        out2 = F.avg_pool2d(out2, 4)
+        out2 = F.avg_pool2d(out2, 2)
         middle2_fea = out2
         out2 = out2.view(out2.size(0), -1)
         out2 = self.ex2linear(out2)
@@ -179,14 +199,14 @@ class VGG19EarlyExit(VGG19):
         out = self.blocks[2](out)
 
         out3 = self.ex3featureextractor(F.relu(out))
-        out3 = F.avg_pool2d(out3, 4)
+        out3 = F.avg_pool2d(out3, 2)
         middle3_fea = out3
         out3 = out3.view(out3.size(0), -1)
         out3 = self.ex3linear(out3)
 
         out = self.blocks[3](out)
 
-        out4 = F.avg_pool2d(out, 4)
+        out4 = F.avg_pool2d(out, 2)
         middle4_fea = out4
         out4 = out4.view(out4.size(0), -1)
         out4 = self.ex4linear(out4)
@@ -194,7 +214,6 @@ class VGG19EarlyExit(VGG19):
         out = self.blocks[4](out)
 
         # ExitEnsemble used avg_pool2d here, but the og didn't...
-        out = F.avg_pool2d(out, 4)
         final_fea = out
         out = out.view(out.size()[0], -1)
         out = self.classifier(out)
@@ -218,7 +237,7 @@ class VGG19MCEarlyExit(VGG19EarlyExit):
                     dropout_layer = MCDropout(self.dropout_p)
                     self.non_sequentialized_blocks[block].append(dropout_layer)
                 elif self.dropout == "layer":
-                    new_block = []
+                    new_block = nn.ModuleList()
                     # Can this be the same object?
                     dropout_layer = MCDropout(self.dropout_p)
                     for layer in self.non_sequentialized_blocks[block]:
@@ -239,6 +258,8 @@ class VGG19MCEarlyExit(VGG19EarlyExit):
                     self.non_sequentialized_classifier[layer] = dropout_layer
             # Overwrite classifier from super class
             self.classifier = nn.Sequential(*self.non_sequentialized_classifier)
+        
+        self.init_weights()
 
 
 
