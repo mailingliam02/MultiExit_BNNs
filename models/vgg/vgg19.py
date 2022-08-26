dc51d5753dc56b3bf1838ff194cfff2d9ec44212
Liam Castelli <liam.castelli21@imperial.ac.uk>
2022-08-26 12:59:49 +0100

    Updated VGG to work with ChestX and Cifar100, with all dropout types (without doubling up on dropout layers)

--- models/vgg/vgg19.py
+++ models/vgg/vgg19.py
@@ -1,4 +1,5 @@
-"""vgg in pytorch
+"""
+https://github.com/weiaicunzai/pytorch-cifar100/blob/master/models/vgg.py
 [1] Karen Simonyan, Andrew Zisserman
     Very Deep Convolutional Networks for Large-Scale Image Recognition.
     https://arxiv.org/abs/1409.1556v6
@@ -17,7 +18,7 @@ def get_vgg_19(ensemble, network_hyperparams):
         model = VGG19(**dict_drop(network_hyperparams,"call", "load_model",
             "resnet_type","dropout","dropout_exit", "dropout_p"))
 
-    if ensemble == "early_exit" or ensemble is None:
+    elif ensemble == "early_exit":
         # n_exits, out_dim
         model = VGG19EarlyExit(**dict_drop(network_hyperparams,"call", "load_model",
             "resnet_type","dropout","dropout_exit", "dropout_p"))
@@ -36,6 +37,7 @@ def get_vgg_19(ensemble, network_hyperparams):
 
     if network_hyperparams["image_size"] == 224 and network_hyperparams["out_dim"] == 7:
         model = load_imagenet_weights(model)
+        check_weight_load(model)
     return model
 
 def load_imagenet_weights(model):
@@ -46,6 +48,19 @@ def load_imagenet_weights(model):
     state_dict = vgg_pytorch_to_new_vgg(state_dict)
     model.load_state_dict(state_dict, strict = False)
     return model
+
+def check_weight_load(model):
+    if type(model) == VGG19 or model.dropout is None or model.dropout == "block":
+        if math.isclose(model.blocks[4][9].weight[0][0][0][0].item(), -0.0026779528707265854):
+            print("Model Weights Loaded")
+        else:
+            print("Not Loaded Correctly!")
+    elif model.dropout == "layer":
+        if math.isclose(model.blocks[4][18].weight[0][0][0][0].item(), -0.0026779528707265854):
+            print("Model Weights Loaded")
+        else:
+            print("Not Loaded Correctly!")
+    return None
 
 def vgg_pytorch_to_new_vgg(state_dict):
     block = 0
@@ -81,7 +96,6 @@ class VGG(nn.Module):
     def init_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                #torch.nn.init.kaiming_uniform_(m.weight, a=0, mode='fan_in', nonlinearity='relu')
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
             elif isinstance(m, nn.BatchNorm2d):
@@ -121,23 +135,36 @@ def make_layers(cfg, batch_norm=False):
 
         layers.append(nn.ReLU(inplace=True))
         input_channel = l
-    non_sequentialized_blocks = blocks
+    sequential_blocks = nn.ModuleList()
     for block in range(len(blocks)):
-        blocks[block] = nn.Sequential(*blocks[block])
-    return (blocks, non_sequentialized_blocks)
+        sequential_blocks.append(nn.Sequential(*blocks[block]))
+    return (sequential_blocks, blocks)
 
 
-def make_classifier(size, num_classes):
+def make_classifier(size, num_classes, mc_dropout_p = 0):
     if size == 224:
-        classifier = nn.ModuleList([nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(p=0.5),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(p=0.5),
-            nn.Linear(4096, num_classes)])
+        if mc_dropout_p == 0:
+            classifier = nn.ModuleList([nn.Linear(512 * 7 * 7, 4096),
+                nn.ReLU(True),
+                nn.Dropout(p=0.5),
+                nn.Linear(4096, 4096),
+                nn.ReLU(True),
+                nn.Dropout(p=0.5),
+                nn.Linear(4096, num_classes)])
+        else:
+            classifier = nn.ModuleList([nn.Linear(512 * 7 * 7, 4096),
+                nn.ReLU(True),
+                MCDropout(p=mc_dropout_p),
+                nn.Linear(4096, 4096),
+                nn.ReLU(True),
+                MCDropout(p=mc_dropout_p),
+                nn.Linear(4096, num_classes)])            
     else:
-        classifier =  nn.ModuleList([nn.Linear(512, num_classes)])
+        if mc_dropout_p == 0:
+            classifier =  nn.ModuleList([nn.Linear(512, num_classes)])
+        else:
+            classifier =  nn.ModuleList([MCDropout(p = mc_dropout_p),
+                            nn.Linear(512, num_classes)])
     return nn.Sequential(*classifier)
 
 
@@ -151,34 +178,45 @@ class VGG19(VGG):
 
 class VGG19MC(VGG19):
     def __init__(self,dropout_exit = False, dropout = None, dropout_p = 0.5, n_exits = 1, out_dim = 100, *args,  **kwargs):
-        super().__init__(n_exits = n_exits, out_dim = out_dim)
+        super().__init__(*args,  **kwargs)
         self.n_exits = n_exits
         self.out_dim = out_dim
         self.dropout = dropout
         self.dropout_p = dropout_p
         self.dropout_exit = dropout_exit
+        dropout_all_blocks = False
+        # Overwrite super classifier
+        if self.dropout_exit:
+            self.classifier = make_classifier(self.image_size,self.out_dim,self.dropout_p)
+        # Load weights
+        self.init_weights()
+        if self.image_size == 224 and self.out_dim == 7:
+            load_imagenet_weights(self)
+            # Additional Linear layers between dropout, so can put dropout at end of all blocks
+            dropout_all_blocks = True
 
         if self.dropout is not None:
             self.blocks = nn.ModuleList()
             for block in range(len(self.non_sequentialized_blocks)):
                 if self.dropout == "block":
-                    dropout_layer = MCDropout(self.dropout_p)
-                    self.non_sequentialized_blocks[block].append(dropout_layer)
+                    if not dropout_all_blocks and block == len(self.non_sequentialized_blocks)-1:
+                        pass
+                    else:
+                        dropout_layer = MCDropout(self.dropout_p)
+                        self.non_sequentialized_blocks[block].append(dropout_layer)
                 elif self.dropout == "layer":
-                    new_block = []
-                    # Can this be the same object?
-                    dropout_layer = MCDropout(self.dropout_p)
-                    for layer in self.non_sequentialized_blocks[block]:
-                        new_block.append(layer)
-                        new_block.append(dropout_layer)
+                    new_block = nn.ModuleList()
+                    for layer_num, layer in enumerate(self.non_sequentialized_blocks[block]):
+                        if (not dropout_all_blocks and block == len(self.non_sequentialized_blocks)-1 
+                        and layer_num == len(self.non_sequentialized_blocks[block])-1):
+                            new_block.append(layer)
+                        else:
+                            new_block.append(layer)
+                            new_block.append(MCDropout(self.dropout_p))
                     self.non_sequentialized_blocks[block] = new_block
             # Overwrite blocks from super class
             for block in range(len(self.non_sequentialized_blocks)):
-                self.blocks[block] = nn.Sequential(*self.non_sequentialized_blocks[block])
-        if self.dropout_exit:
-            self.classifier = nn.Sequential(MCDropout(self.dropout_p),self.classifier)
-        
-        self.init_weights()
+                self.blocks.append(nn.Sequential(*self.non_sequentialized_blocks[block]))
 
     def forward(self, x):
         output = x
@@ -192,8 +230,8 @@ class VGG19MC(VGG19):
         return [output]
 
 class VGG19EarlyExit(VGG19):
-    def __init__(self,n_exits = 4, out_dim = 100, *args,  **kwargs):
-        super().__init__(n_exits = n_exits, out_dim = out_dim, *args,  **kwargs)
+    def __init__(self, *args,  **kwargs):
+        super().__init__(*args,  **kwargs)
         ex1conv1 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=False)
         ex1conv2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=False)
         ex1conv3 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False)
@@ -205,7 +243,7 @@ class VGG19EarlyExit(VGG19):
         ex1relu3 = nn.ReLU(inplace=True)
         ex1featureextractor_list = nn.ModuleList(modules = [ex1conv1,ex1bn1,ex1relu1,ex1conv2,ex1bn2,ex1relu2,ex1conv3,ex1bn3,ex1relu3])
         self.ex1featureextractor = nn.Sequential(*ex1featureextractor_list)
-        self.ex1linear = make_classifier(self.image_size, out_dim) 
+        self.ex1linear = make_classifier(self.image_size, self.out_dim) 
 
         ex2conv1 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=False)
         ex2conv2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False)
@@ -215,7 +253,7 @@ class VGG19EarlyExit(VGG19):
         ex2relu2 = nn.ReLU(inplace=True)
         ex2featureextractor_list = nn.ModuleList(modules = [ex2conv1,ex2bn1,ex2relu1,ex2conv2,ex2bn2,ex2relu2])
         self.ex2featureextractor = nn.Sequential(*ex2featureextractor_list)
-        self.ex2linear = make_classifier(self.image_size, out_dim) 
+        self.ex2linear = make_classifier(self.image_size, self.out_dim) 
 
 
         ex3conv1 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False)
@@ -223,91 +261,94 @@ class VGG19EarlyExit(VGG19):
         ex3relu1 = nn.ReLU(inplace=True)
         ex3featureextractor_list = nn.ModuleList(modules = [ex3conv1,ex3bn1,ex3relu1])
         self.ex3featureextractor = nn.Sequential(*ex3featureextractor_list)
-        self.ex3linear = make_classifier(self.image_size, out_dim) 
+        self.ex3linear = make_classifier(self.image_size, self.out_dim) 
 
-        self.ex4linear = make_classifier(self.image_size, out_dim) 
+        self.ex4linear = make_classifier(self.image_size, self.out_dim) 
 
         self.init_weights()
 
     def forward(self, x):
-        #print("Input",x)
         out = self.blocks[0](x)
-        #print("Block1",out)
         out1 = self.ex1featureextractor(F.relu(out))
         out1 = F.avg_pool2d(out1, 2)
         middle1_fea = out1
         out1 = out1.view(out1.size(0), -1)
         out1 = self.ex1linear(out1)
-        #print("Exit1",out1)
         out = self.blocks[1](out)
-        #print("Block2",out)
         out2 = self.ex2featureextractor(F.relu(out))
         out2 = F.avg_pool2d(out2, 2)
         middle2_fea = out2
         out2 = out2.view(out2.size(0), -1)
         out2 = self.ex2linear(out2)
-        #print("Exit2",out2)
         out = self.blocks[2](out)
-        #print("Block3",out)
         out3 = self.ex3featureextractor(F.relu(out))
         out3 = F.avg_pool2d(out3, 2)
         middle3_fea = out3
         out3 = out3.view(out3.size(0), -1)
         out3 = self.ex3linear(out3)
-        #print("Exit3",out3)
         out = self.blocks[3](out)
-        #print("Block4",out)
         out4 = F.avg_pool2d(out, 2)
         middle4_fea = out4
         out4 = out4.view(out4.size(0), -1)
         out4 = self.ex4linear(out4)
-        #print("Exit4",out4)
         out = self.blocks[4](out)
-        #print("Block5",out)
         # ExitEnsemble used avg_pool2d here, but the og didn't...
         final_fea = out
         out = out.view(out.size()[0], -1)
         out = self.classifier(out)
-        #print("Classifier",out)
         self.intermediary_output_list = (out, [out1, out2, out3, out4], final_fea, [middle1_fea, middle2_fea, middle3_fea, middle4_fea])
         return [out1, out2, out3, out4, out]
 
 
 class VGG19MCEarlyExit(VGG19EarlyExit):
     def __init__(self,dropout_exit = False, dropout = None, dropout_p = 0.5, n_exits = 4, out_dim = 100, *args,  **kwargs):
-        super().__init__(n_exits = n_exits, out_dim = out_dim)
+        super().__init__(n_exits = n_exits, out_dim = out_dim, *args,  **kwargs)
         self.n_exits = n_exits
         self.out_dim = out_dim
         self.dropout = dropout
         self.dropout_p = dropout_p
         self.dropout_exit = dropout_exit
+        # Same as in MC
+        dropout_all_blocks = False
+
+        # Update exits so can get init correctly
+        if self.dropout_exit:
+            # Remake all exits with dropout
+            self.ex1linear = make_classifier(self.image_size,self.out_dim,self.dropout_p)
+            self.ex2linear = make_classifier(self.image_size,self.out_dim,self.dropout_p)
+            self.ex3linear = make_classifier(self.image_size,self.out_dim,self.dropout_p)
+            self.ex4linear = make_classifier(self.image_size,self.out_dim,self.dropout_p)
+            # Overwrite classifier from super class
+            self.classifier = make_classifier(self.image_size,self.out_dim,self.dropout_p)
+        
+        # Load weights
+        self.init_weights()
+        if self.image_size == 224 and self.out_dim == 7:
+            load_imagenet_weights(self)
+            dropout_all_blocks = True
 
         if self.dropout is not None:
+            self.blocks = nn.ModuleList()
             for block in range(len(self.non_sequentialized_blocks)):
                 if self.dropout == "block":
-                    dropout_layer = MCDropout(self.dropout_p)
-                    self.non_sequentialized_blocks[block].append(dropout_layer)
+                    if not dropout_all_blocks and (block == len(self.non_sequentialized_blocks)-1 or block == len(self.non_sequentialized_blocks)-2):
+                        pass
+                    else:
+                        dropout_layer = MCDropout(self.dropout_p)
+                        self.non_sequentialized_blocks[block].append(dropout_layer)
                 elif self.dropout == "layer":
                     new_block = nn.ModuleList()
-                    # Can this be the same object?
-                    for layer in self.non_sequentialized_blocks[block]:
-                        new_block.append(layer)
-                        new_block.append(MCDropout(self.dropout_p))
+                    for layer_num, layer in enumerate(self.non_sequentialized_blocks[block]):
+                        if not dropout_all_blocks and (block == len(self.non_sequentialized_blocks)-1 
+                            or block == len(self.non_sequentialized_blocks)-2) and layer_num == len(self.non_sequentialized_blocks[block])-1:
+                            new_block.append(layer)
+                        else:
+                            new_block.append(layer)
+                            new_block.append(MCDropout(self.dropout_p))
                     self.non_sequentialized_blocks[block] = new_block
             # Overwrite blocks from super class
             for block in range(len(self.non_sequentialized_blocks)):
-                self.blocks[block] = nn.Sequential(*self.non_sequentialized_blocks[block])
-
-        if self.dropout_exit:
-            self.ex1linear = nn.Sequential(MCDropout(self.dropout_p), self.ex1linear)
-            self.ex2linear = nn.Sequential(MCDropout(self.dropout_p), self.ex2linear)
-            self.ex3linear = nn.Sequential(MCDropout(self.dropout_p), self.ex3linear)
-            self.ex4linear = nn.Sequential(MCDropout(self.dropout_p), self.ex4linear)
-            # Overwrite classifier from super class
-            self.classifier = nn.Sequential(MCDropout(self.dropout_p),self.classifier)
-        
-        self.init_weights()
-
+                self.blocks.append(nn.Sequential(*self.non_sequentialized_blocks[block]))
 
 class MCDropout(nn.Dropout):
 
